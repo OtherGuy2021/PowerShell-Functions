bcdd6a9ac5b43036359d8264cd1a1ddb1e7f0e99
kieranwalsh <walshk@pm.me>
2021-03-25 12:20:34 +0000

    Uploaded 'get-FormattedDate' function

---
+++ get-FormattedDate.ps1
@@ -0,0 +1,35 @@
+﻿function get-FormattedDate
+{
+    [CmdletBinding()]
+    [OutputType([String])]
+    Param
+    (
+        [Parameter(Mandatory = $false,
+                ValueFromPipelineByPropertyName = $true,
+        Position = 0)]
+        $Date = (Get-Date)
+    )
+    $DaySuffix = switch -regex ($Date.Day.ToString()) {
+        '1(1|2|3)$' 
+        {
+            'th'
+        }
+        '.?1$'      
+        {
+            'st'
+        }
+        '.?2$'      
+        {
+            'nd'
+        }
+        '.?3$'      
+        {
+            'rd'
+        }
+        default
+        {
+            'th'
+        }
+    }
+    '{0}, {1:MMMM} {2}{3}, {4}' -f $Date.DayOfWeek, $Date, $Date.Day, $DaySuffix, $Date.Year
+}
