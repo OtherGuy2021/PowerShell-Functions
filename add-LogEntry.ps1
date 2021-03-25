87fdd3f5b109b6e1e2f09a644b91bd3c12369103
kieranwalsh <walshk@pm.me>
2021-03-25 14:10:56 +0000

    New function 'add-LogEntry'

---
+++ add-LogEntry.ps1
@@ -0,0 +1,10 @@
+﻿function add-LogEntry
+{
+    [CmdletBinding()]
+    Param
+    (
+        [string]$Output
+    )
+    $Output
+    "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')`t $Output" | Out-File -FilePath $LogFile -Encoding 'utf8' -Append
+}
