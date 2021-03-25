ec1a2db1d66139a82dc1e8bc2738f78eb2c7eeea
kieranwalsh <walshk@pm.me>
2021-03-25 15:28:12 +0000

    Updated the ReadMe content.

---
+++ remove-Diacritics.ps1
@@ -0,0 +1,10 @@
+﻿function remove-Diacritics 
+{
+    [CmdletBinding()]
+    Param
+    (
+        # https://en.wikipedia.org/wiki/Diacritic
+        [string]$Name
+    )
+    [Text.Encoding]::ASCII.GetString([Text.Encoding]::GetEncoding('Cyrillic').GetBytes($Name))
+}
