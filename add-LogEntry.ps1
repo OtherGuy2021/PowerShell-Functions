832e70c8a5a4af37b485244d39c9eb68f4b1e6a5
kieranwalsh <walshk@pm.me>
2021-04-29 13:30:20 +0100

    Updated add-LogEntry

--- add-LogEntry.ps1
+++ add-LogEntry.ps1
@@ -9,34 +9,6 @@ function add-LogEntry
         [switch]$Indent,
         [switch]$IsError
     )
-    <#
-    .SYNOPSIS
-    This function outputs information to a logfile and to the powershell host.
-
-    .DESCRIPTION
-    The function is similar to Tee-Object in that it saves items to a logfile, but also outputs the data to the screen.
-    If you're at the screen you can watch the data in real time but the limitiation of Tee-Object is that checking logfiles
-    at a later date meant that you
-
-    .PARAMETER Output
-    Parameter description
-
-    .PARAMETER Indent
-    Parameter description
-
-    .PARAMETER IsError
-    Indicates that the entry is to be marked as an error and prefixes it with '****'.
-
-    .EXAMPLE
-    An example
-
-    .NOTES
-        Filename: add-LogEntry.ps1
-        Contributors: Kieran Walsh
-        Created: 2018-05-23
-        Last Updated: 2021-04-29
-        Version: 1.01.0
-    #>
 
     if($Indent)
     {
